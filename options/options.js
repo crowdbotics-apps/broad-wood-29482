6aabd6ba910f94c8edd8d0a5e75d3e8fbc58a6cb
Crowdbotics <team@crowdbotics.com>
2021-08-05 00:26:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broad_wood_29482Identifier",
+  url: "https://broad_wood_29482Identifier.botics.co",
+  api: "https://broad_wood_29482Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
